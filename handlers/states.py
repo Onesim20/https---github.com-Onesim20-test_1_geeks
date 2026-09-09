f79c6502d0847c70b04a3fe71eeaf827620f2c62
Onesim20 <t40804069@gmail.com>
2026-09-09 12:14:20 +0600

    из.
    Removed unnecessary comment from AddDrink class.

--- handlers/states.py
+++ handlers/states.py
@@ -2,7 +2,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class AddDrink(StatesGroup):
-    """Состояния диалога добавления напитка (/add_product)."""
+
 
     name = State()
     price = State()
